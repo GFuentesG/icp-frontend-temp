d9d5c41bb92572bd20a4d1ea63b3a632ee80c870
GFuentesG <g7fuentes7@gmail.com>
2024-12-20 13:43:55 -0500

    agregando paquete react-router-dom

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import Apptemp from "./Apptemp"
-
+import Apptemp from "./Apptemp";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
 
   return (
 
-    <Apptemp />
+    <BrowserRouter>
+      <Navbar />
+      <Apptemp />
+    </BrowserRouter>
+    
     
   );
 }
