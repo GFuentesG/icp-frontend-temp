6981856d312245aa8f713f6bd23f4c56dc51f660
GFuentesG <g7fuentes7@gmail.com>
2024-12-20 22:13:21 -0500

    activando boton login en navbar

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import Apptemp from "./Apptemp";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
 
   return (
 
-    <BrowserRouter>
-      <Navbar />
-      <Apptemp />
-    </BrowserRouter>
-    
-    
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Apptemp />
+      </BrowserRouter>
+    </AuthProvider >
+
+
   );
 }
 
--- src/frontend/src/components/navbar/Navbar.jsx
+++ src/frontend/src/components/navbar/Navbar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
+import { AuthContext } from '../../context/AuthContext';
 
 const Navbar = () => {
+
+    const { isAuthenticated, login, logout } = useContext(AuthContext)
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+        logout();
+        
+    }
     return (
         <nav className={styles.navbar}>
             <div>
@@ -11,10 +20,17 @@ const Navbar = () => {
 
             <ul className={styles.navbarLinks}>
 
-                <li><Link to='/'>Inicio</Link></li>
-                <li><Link to='cart'>Carrito</Link></li>
-                <li><Link to='contact'>Contacto</Link></li>
-                <li><Link to='about'>Acerca de</Link></li>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='cart'>Cart</Link></li>
+                <li><Link to='contact'>Contact</Link></li>
+                <li><Link to='about'>About</Link></li>
+                {/* {isAuthenticated ? <LogoutButton /> : <LoginButton />} */}
+                {!isAuthenticated ? (
+                    <li><Link to='/' onClick={login}>Login</Link></li>
+                ) : (
+                    <li><Link to='/' onClick={handleLogout}>Logout</Link></li>
+                )}
+
             </ul>
         </nav>
     )
