cba1660860358c3881db75a3fa4bcc18f94fb4a8
GFuentesG <g7fuentes7@gmail.com>
2024-12-20 19:26:55 -0500

    reconfigurando logo navbar

--- src/frontend/src/components/navbar/Navbar.jsx
+++ src/frontend/src/components/navbar/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css';
 
 const Navbar = () => {
-  return (
-    <nav>
-        <ul>
-            <li><Link to='/'>Inicio</Link></li>
-        </ul>
-    </nav>
-  )
+    return (
+        <nav className={styles.navbar}>
+            <div>
+                <img src="/icpecommerce2.jpg" className={styles.navbarlogo} alt="logo" />
+            </div>
+
+            <ul className={styles.navbarLinks}>
+
+                <li><Link to='/'>Inicio</Link></li>
+                <li><Link to='cart'>Carrito</Link></li>
+                <li><Link to='contact'>Contacto</Link></li>
+                <li><Link to='about'>Acerca de</Link></li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
